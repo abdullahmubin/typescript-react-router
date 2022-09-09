2742139f01e6eaa5e4d6a1f7d3eb785eadda5387
abdullahmubin <amubin19@gmail.com>
2022-09-09 21:41:52 +0600

    display CV completed.

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,212 @@ import logo from './logo.svg';
 import './App.css';
 import './base.scss'
 import './assets/css/styles.scss';
-import './assets/css/firsttemplate.css'; 
+import './assets/css/firsttemplate.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { actionCreators } from './state';
 import { RootState } from './state/reducers';
 
-import Navibar  from './components/Navbar'
+import Navibar from './components/Navbar'
 
-import Home  from './components/Home'
+import Home from './components/Home'
 import FirstTemp from './components/TemplateList/FirstTemp'
 
+let testConst = {
+  idMain:1,
+  necessaryColor: "Super hero squad",
+  title: "Jonney Anderson",
+  subtitle: "Web Designer",
+  profilePic: "Super tower",
+  master: true,
+  reference: [
+    {
+      id:1,
+      idMain: 1,
+      name: "Jonney Smith",
+      designation: "CEO Casinocarol",
+      mail: "Dan Jukes",
+      mobile: "+00 890 1232 8767",
+      occupation: "",
+    }
+  ],
+  hobbies: [
+    {
+      id:1,
+      idMain: 1,
+      name: "Writing",
+      image: "fas fa-pencil-alt",
+      defaultImg: "fas fa-pencil-alt"
+    },
+    {
+      id:2,
+      idMain: 1,
+      name: "Cycling",
+      image: "fas fa-bicycle",
+      defaultImg: "fas fa-bicycle"
+    },
+    {
+      id:3,
+      idMain: 1,
+      name: "Football",
+      image: "fas fa-futbol",
+      defaultImg: "fas fa-futbol"
+    },
+    {
+      id:4,
+      idMain: 1,
+      name: "Movies",
+      image: "fas fa-film",
+      defaultImg: "fas fa-film"
+    },
+    {
+      id:5,
+      idMain: 1,
+      name: "Travel",
+      image: "fas fa-plane-departure",
+      defaultImg: "fas fa-plane-departure"
+    },
+    {
+      id:6,
+      idMain: 1,
+      name: "Games",
+      image: "fas fa-gamepad",
+      defaultImg: "fas fa-gamepad"
+    }
+  ],
+  "education": [
+    {
+      id:1,
+      idMain: 1,
+      "timeline": "2013-2015",
+      "degree": "Master Degree",
+      "school": "Cambridg University"
+    },
+    {
+      id:2,
+      idMain: 1,
+      "timeline": "2013-2015",
+      "degree": "Master Degree",
+      "school": "Cambridg University"
+    },
+    {
+      id:3,
+      idMain: 1,
+      "timeline": "2013-2015",
+      "degree": "Master Degree",
+      "school": "Cambridg University"
+    },
+    {
+      id:4,
+      idMain: 1,
+      "timeline": "2013-2015",
+      "degree": "Master Degree",
+      "school": "Cambridg University"
+    }
+  ],
+  "profileTxt": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis accumsan purus enim, a vestibulum est tristique sit amet. Suspendisse nibh nisl, imperdiet sit amet mi vitae, elementum elementum nibh. Vivamus vitae eros malesuada, convallis dolor malesuada, lobortis ex. Sed cursus augue risus, ac semper est consectetur vitae. Praesent consequat metus sit amet rhoncus luctus.",
+  "workExperience": [
+    {
+      id:1,
+      idMain: 1,
+      "designation": "Junior Software Developer",
+      "compancy": "Microsoft / United States",
+      "timeline": "2008-2011",
+      "taskList": [
+        {
+          id:1,
+          idWorkExperience: 1,
+          "title": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+          "description": "Molecule Man"
+        },
+        {
+          id:2,
+          idWorkExperience: 1,
+          "title": "Sorem dolor sit amet, consectetur.",
+          "description": "Molecule Man"
+        }
+      ]
+    },
+    {
+      id:2,
+      idMain: 1,
+      "designation": "Junior Software Developer",
+      "compancy": "Microsoft / United States",
+      "timeline": "2008-2011",
+      "taskList": [
+        {
+          id:3,
+          idWorkExperience: 2,
+          "title": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+          "description": "Molecule Man"
+        },
+        {
+          id:4,
+          idWorkExperience: 2,
+          "title": "Sorem dolor sit amet, consectetur.",
+          "description": "Molecule Man"
+        }
+      ]
+    },
+    {
+      id:3,
+      idMain: 1,
+      "designation": "Junior Software Developer",
+      "compancy": "Microsoft / United States",
+      "timeline": "2008-2011",
+      "taskList": [
+        {
+          id:5,
+          idWorkExperience: 3,
+          "title": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+          "description": "Molecule Man"
+        },
+        {
+          id:6,
+          idWorkExperience: 3,
+          "title": "Sorem dolor sit amet, consectetur.",
+          "description": "Molecule Man"
+        }
+      ]
+    }
+  ],
+  "skills": [
+    {
+      id:1,
+      idMain: 1,
+      "name": "Photoshop",
+      "percentage": "60"
+    },
+    {
+      id:2,
+      idMain: 1,
+      "name": "PHP",
+      "percentage": "60"
+    },
+    {
+      id:3,
+      idMain: 1,
+      "name": "Web Design",
+      "percentage": "60"
+    },
+    {
+      id:4,
+      idMain: 1,
+      "name": "Web Development",
+      "percentage": "60"
+    }
+  ],
+  "contact": {
+    "firtMobile": "+123 8767 5465",
+    "SecondMobile": "+122 2345 3763",
+    "email": "info@smarteyeapps.com",
+    "website": "www.smarteyeapps.com",
+    "Linkedin": "Dan Jukes",
+    "git": "Dan Jukes",
+    "facebook": "Dan Jukes",
+    "address": "First Floor,Vincent Plaza, Toranto, Canada"
+  }
+}
 
 
 
@@ -119,11 +314,12 @@ function PostLists() {
 function Post() {
   const { slug } = useParams();
 
+
   return (
     <div style={{ padding: 20 }}>
       <h3>Number of post:- {slug}</h3>
-      
-      <FirstTemp />
+
+      <FirstTemp CVList={testConst} />
     </div>
   );
 }
--- src/components/TemplateList/FirstTemp/index.tsx
+++ src/components/TemplateList/FirstTemp/index.tsx
@@ -19,7 +19,96 @@ const colors = [
     { color: 'rgb(239, 7, 223)' },
 ]
 
-const FirstTemp: React.FC = () => {
+interface referenceIf {
+    reference: {
+        id: number,
+        idMain: number,
+        name?: string,
+        designation?: string,
+        mail?: string,
+        mobile?: string,
+        occupation?: string
+    }
+}
+
+interface hobbieIn {
+    hobbie: {
+        id: number,
+        idMain: number,
+        name?: string,
+        image?: string,
+        defaultImg?: string
+
+    }
+}
+
+interface educationIn {
+    education: {
+        id: number,
+        idMain: number,
+        timeline?: string,
+        degree?: string,
+        school?: string
+    }
+}
+
+interface workExperienceIn {
+    workExperience: {
+        id: number,
+        idMain: number,
+        designation?: string,
+        compancy?: string,
+        timeline?: string,
+        taskList?: taskListIn['taskList'][]
+    }
+}
+
+interface taskListIn {
+    taskList: {
+        id: number,
+        idWorkExperience: number,
+        title?: string,
+        description?: string
+    }
+}
+interface cVInterface {
+    CVList: {
+        idMain: number,
+        necessaryColor?: string,
+        title?: string,
+        subtitle?: string,
+        profilePic?: string,
+        master: boolean,
+        reference?: referenceIf['reference'][]
+        hobbies?: hobbieIn['hobbie'][]
+        education?: educationIn['education'][]
+        profileTxt?: string,
+        workExperience?: workExperienceIn['workExperience'][],
+        skills?: {
+            id: number,
+            idMain: number,
+            name?: string,
+            percentage?: string
+        }[],
+        contact?: {
+            firtMobile?: string,
+            SecondMobile?: string,
+            email?: string,
+            website?: string,
+            Linkedin?: string,
+            git?: string,
+            facebook?: string,
+            address?: string
+        }
+    },
+
+
+}
+
+const FirstTemp = (props: cVInterface) => {
+    console.log(`cVInterface cVInterface`)
+    console.log(props.CVList)
+    let data = props.CVList
     return (
 
         <>
@@ -31,8 +120,8 @@ const FirstTemp: React.FC = () => {
                             <div className="left-side">
                                 <div className="profile-info">
                                     <img src={profile} alt="" />
-                                    <h3>Jonney Anderson</h3>
-                                    <span>Web Designer</span>
+                                    <h3>{data.title}</h3>
+                                    <span>{data.subtitle}</span>
                                 </div>
                                 <h4 className="ltitle">Contact</h4>
                                 <div className="contact-box pb0">
@@ -40,8 +129,8 @@ const FirstTemp: React.FC = () => {
                                         <i className="fas fa-phone"></i>
                                     </div>
                                     <div className="detail">
-                                        +123 8767 5465 <br />
-                                        +122 2345 3763
+                                        {data.contact?.firtMobile} <br />
+                                        {data.contact?.SecondMobile}
                                     </div>
                                 </div>
                                 <div className="contact-box pb0">
@@ -49,8 +138,8 @@ const FirstTemp: React.FC = () => {
                                         <i className="fas fa-globe-americas"></i>
                                     </div>
                                     <div className="detail">
-                                        info@smarteyeapps.com <br />
-                                        www.smarteyeapps.com
+                                        {data.contact?.email} <br />
+                                        {data.contact?.website}
                                     </div>
                                 </div>
                                 <div className="contact-box">
@@ -58,7 +147,7 @@ const FirstTemp: React.FC = () => {
                                         <i className="fas fa-map-marker-alt"></i>
                                     </div>
                                     <div className="detail">
-                                        First Floor,Vincent Plaza, Toranto, Canada
+                                        {data.contact?.address}
                                     </div>
                                 </div>
                                 <h4 className="ltitle">Contact</h4>
@@ -71,36 +160,51 @@ const FirstTemp: React.FC = () => {
                                 </ul>
                                 <h4 className="ltitle">Referencess</h4>
 
-                                <div className="refer-cov">
-                                    <b>Jonney Smith</b>
-                                    <p>CEO Casinocarol</p>
-                                    <span>p +00 890 1232 8767</span>
-                                </div>
-                                <div className="refer-cov">
+                                {
+                                    data.reference?.map((i) => {
+                                        return (
+                                            <div className="refer-cov">
+                                                <b>{i.name}</b>
+                                                <p>{i.designation}</p>
+                                                <span>{i.mobile}</span>
+                                            </div>
+                                        )
+                                    })
+                                }
+
+                                {/* <div className="refer-cov">
                                     <b>Jonney Smith</b>
                                     <p>System Administrator</p>
                                     <span>p +00 890 1232 8767</span>
-                                </div>
+                                </div> */}
                                 <h4 className="ltitle">Hobbies</h4>
                                 <ul className="hoby row no-margin">
-                                    <li><i className="fas fa-pencil-alt"></i> <br /> Writing</li>
-                                    <li><i className="fas fa-bicycle"></i> <br /> Cycling</li>
+                                    {
+                                        data.hobbies?.map((i) => {
+                                            return (
+                                                <li><i className={`${i.defaultImg}`}></i> <br /> {i.name}</li>
+                                            )
+                                        })
+
+                                    }
+                                    {/* <li><i className="fas fa-futbol"></i> <br /> Football</li> */}
+                                    {/* <li><i className="fas fa-bicycle"></i> <br /> Cycling</li>
                                     <li><i className="fas fa-futbol"></i> <br /> Football</li>
                                     <li><i className="fas fa-film"></i><br /> Movies</li>
                                     <li><i className="fas fa-plane-departure"></i> <br />Travel</li>
-                                    <li><i className="fas fa-gamepad"></i> <br /> Games</li>
+                                    <li><i className="fas fa-gamepad"></i> <br /> Games</li> */}
                                 </ul>
                             </div>
                         </div>
                         <div className="col-md-8 rt-div">
                             <div className="rit-cover">
                                 <div className="hotkey">
-                                    <h1 className="">Jonney Anderson </h1>
-                                    <small>Web Designer</small>
+                                    <h1 className="">{data.title} </h1>
+                                    <small>{data.subtitle}</small>
                                 </div>
                                 <h2 className="rit-titl"><i className="far fa-user"></i> Profile</h2>
                                 <div className="about">
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis accumsan purus enim, a vestibulum est tristique sit amet. Suspendisse nibh nisl, imperdiet sit amet mi vitae, elementum elementum nibh. Vivamus vitae eros malesuada, convallis dolor malesuada, lobortis ex. Sed cursus augue risus, ac semper est consectetur vitae. Praesent consequat metus sit amet rhoncus luctus.</p>
+                                    <p>{data.profileTxt}</p>
                                     <div className="btn-ro row no-margin">
                                         <ul className="btn-link">
                                             <li>
@@ -114,103 +218,64 @@ const FirstTemp: React.FC = () => {
                                 </div>
 
                                 <h2 className="rit-titl"><i className="fas fa-briefcase"></i> Work Experiance</h2>
-                                <div className="work-exp">
-                                    <h6>Junior Software Developer <span>2008-2011</span></h6>
-                                    <i>Microsoft / United States</i>
-                                    <ul>
-                                        <li><i className="far fa-hand-point-right"></i> Lorem ipsum dolor sit amet, consectetur adipiscing elit. </li>
-                                        <li><i className="far fa-hand-point-right"></i> Sorem dolor sit amet, consectetur. </li>
-                                        <li><i className="far fa-hand-point-right"></i> Porem ipsum sit amet, consectetur adipiscing </li>
-                                    </ul>
-                                </div>
-                                <div className="work-exp">
-                                    <h6>Junior Software Developer <span>2008-2011</span></h6>
-                                    <i>Microsoft / United States</i>
-                                    <ul>
-                                        <li><i className="far fa-hand-point-right"></i> Lorem ipsum dolor sit amet, consectetur adipiscing elit. </li>
+                                {
+                                    data.workExperience?.map((i) => {
+                                        return (
+                                            <div className="work-exp">
+                                                <h6>{i.designation} <span>{i.timeline}</span></h6>
+                                                <i>{i.compancy}</i>
+                                                <ul>
+                                                    {
+                                                        i.taskList?.map((j) => {
+                                                            return (
+                                                                <li><i className="far fa-hand-point-right"></i> {j.title} </li>
 
-                                        <li><i className="far fa-hand-point-right"></i> Sed cursus augue risus, ac semper est consectetur vitae </li>
-                                    </ul>
-                                </div>
-                                <div className="work-exp">
-                                    <h6>Junior Software Developer <span>2008-2011</span></h6>
-                                    <i>Microsoft / United States</i>
-                                    <ul>
-                                        <li><i className="far fa-hand-point-right"></i> Praesent consequat metus sit amet rhoncus luctus.
+                                                            )
+                                                        })
+                                                    }
+                                                </ul>
+                                            </div>
+                                        )
+                                    })
+                                }
 
-                                        </li>
-                                        <li><i className="far fa-hand-point-right"></i> Lorem ipsum dolor sit amet, consectetur. </li>
-
-                                    </ul>
-                                </div>
 
                                 <h2 className="rit-titl"><i className="fas fa-graduation-cap"></i> Education</h2>
                                 <div className="education">
                                     <ul className="row no-margin">
-                                        <li className="col-md-6"><span>2013-2015</span> <br />
-                                            Master Degree - Cambridg University</li>
-                                        <li className="col-md-6"><span>2013-2015</span> <br />
-                                            Master Degree - Cambridg University</li>
-                                        <li className="col-md-6"><span>2013-2015</span> <br />
-                                            Master Degree - Cambridg University</li>
-                                        <li className="col-md-6"><span>2013-2015</span> <br />
-                                            Master Degree - Cambridg University</li>
+                                        {
+                                            data.education?.map((i) => {
+                                                return (
+                                                    <li className="col-md-6"><span>{i.timeline}</span> <br />
+                                                        {i.degree} - {i.school}</li>
+                                                )
+                                            })
+                                        }
+
+
                                     </ul>
                                 </div>
 
                                 <h2 className="rit-titl"><i className="fas fa-users-cog"></i> Skills</h2>
                                 <div className="profess-cover row no-margin">
-                                    <div className="col-md-6">
-                                        <div className=" prog-row row">
-                                            <div className="col-sm-6">
-                                                Photoshop
-                                            </div>
-                                            <div className="col-sm-6">
-                                                <div className="progress">
-                                                    <div className="progress-bar" role="progressbar" style={{ width: "75%" }}></div>
+                                    {
+                                        data.skills?.map((i) => {
+                                            return (
+                                                <div className="col-md-6">
+                                                    <div className=" prog-row row">
+                                                        <div className="col-sm-6">
+                                                            {i.name}
+                                                        </div>
+                                                        <div className="col-sm-6">
+                                                            <div className="progress">
+                                                                <div className="progress-bar" role="progressbar" style={{ width: `${i.percentage}%` }}></div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
                                                 </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="col-md-6">
-                                        <div className="row prog-row">
-                                            <div className="col-sm-6">
-                                                PHP
-                                            </div>
-                                            <div className="col-sm-6">
-                                                <div className="progress">
-                                                    <div className="progress-bar" role="progressbar" style={{ width: "75%" }}></div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <div className="col-md-6">
-                                        <div className="row prog-row">
-                                            <div className="col-sm-6">
-                                                Web Design
-                                            </div>
-                                            <div className="col-sm-6">
-                                                <div className="progress">
-                                                    <div className="progress-bar" role="progressbar" style={{ width: "75%" }}></div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="col-md-6">
-                                        <div className="row prog-row">
-                                            <div className="col-sm-6">
-                                                Web Development
-                                            </div>
-                                            <div className="col-sm-6">
-                                                <div className="progress">
-                                                    <div className="progress-bar" role="progressbar" style={{ width: "75%" }}></div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-
-
+                                            )
+                                        })
+                                    }
 
 
 
